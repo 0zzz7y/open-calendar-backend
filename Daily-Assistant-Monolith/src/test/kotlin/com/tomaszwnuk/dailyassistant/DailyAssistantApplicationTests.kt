7cc7c264af288b8d072c78ad6423b79d87d9b743
ozzz7y <0zzz7y@gmail.com>
2025-04-02 15:18:22 +0200

    Init

---
+++ Daily-Assistant-Monolith/src/test/kotlin/com/tomaszwnuk/dailyassistant/DailyAssistantApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tomaszwnuk.dailyassistant
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DailyAssistantApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
