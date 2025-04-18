4add2bd5640c4ab7ec1800d8ab9f475d5206e113
ozzz7y <0zzz7y@gmail.com>
2025-04-18 19:17:17 +0200

    Constraints

--- src/main/kotlin/com/tomaszwnuk/dailyassistant/validation/CommunicationConstraints.kt
+++ src/main/kotlin/com/tomaszwnuk/dailyassistant/validation/CommunicationConstraints.kt
@@ -5,6 +5,6 @@ object CommunicationConstraints {
 
     const val MAXIMUM_REQUEST_SIZE: Long = 1024 * 1024
 
-    const val MAXIMUM_REQUESTS_PER_MINUTE: Long = 50
+    const val MAXIMUM_REQUESTS_PER_MINUTE: Long = 1024
 
 }
