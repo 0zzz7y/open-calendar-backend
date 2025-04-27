bc64b05c27cf9d30b83ab428833429b0ce03752b
ozzz7y <0zzz7y@gmail.com>
2025-04-27 21:50:43 +0200

    Cors config

--- src/main/kotlin/com/tomaszwnuk/opencalendar/communication/CommunicationConstants.kt
+++ src/main/kotlin/com/tomaszwnuk/opencalendar/communication/CommunicationConstants.kt
@@ -2,6 +2,8 @@ package com.tomaszwnuk.opencalendar.communication
 
 object CommunicationConstants {
 
-    const val FRONTEND_URL = "http://localhost:5173"
+    const val FRONTEND_PRODUCTION_URL = "https://open-calendar-frontend.onrender.com"
+
+    const val FRONTEND_DEVELOPMENT_URL = "http://localhost:5137"
 
 }
--- src/main/kotlin/com/tomaszwnuk/opencalendar/configuration/CorsConfiguration.kt
+++ src/main/kotlin/com/tomaszwnuk/opencalendar/configuration/CorsConfiguration.kt
@@ -1,6 +1,7 @@
 package com.tomaszwnuk.opencalendar.configuration
 
-import com.tomaszwnuk.opencalendar.communication.CommunicationConstants.FRONTEND_URL
+import com.tomaszwnuk.opencalendar.communication.CommunicationConstants.FRONTEND_DEVELOPMENT_URL
+import com.tomaszwnuk.opencalendar.communication.CommunicationConstants.FRONTEND_PRODUCTION_URL
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
@@ -11,7 +12,7 @@ class CorsConfiguration : WebMvcConfigurer {
 
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
-            .allowedOriginPatterns(FRONTEND_URL)
+            .allowedOriginPatterns(FRONTEND_PRODUCTION_URL, FRONTEND_DEVELOPMENT_URL)
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true)
