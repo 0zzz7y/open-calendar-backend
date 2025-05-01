3566e4ed4b74171d17bf1f59376f545d7813668c
ozzz7y <0zzz7y@gmail.com>
2025-05-01 05:15:21 +0200

    Formatting

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,6 @@ plugins {
     kotlin("plugin.jpa") version "1.9.25"
     id("org.springframework.boot") version "3.4.4"
     id("io.spring.dependency-management") version "1.1.7"
-    id("org.jlleitschuh.gradle.ktlint") version "11.6.1"
 }
 
 dependencies {
